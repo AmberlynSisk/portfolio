d29f36dd4046f2f2d708e4490e5a45407c7d17b9
AmberlynSisk <amberlynsisk@yahoo.com>
2022-06-21 19:43:30 -0500

    animation updates

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RiDownloadLine } from 'react-icons/ri';
 import AboutPic from '../images/about.jpg';
-// import Roll from 'react-reveal/Roll';
+import { Roll } from "react-awesome-reveal";
 import Resume from '../downloads/Amberlyn_Sisk_Resume.PDF';
 
 const About = () => {
@@ -17,11 +17,11 @@ const About = () => {
             <img src={AboutPic} alt="about-pic" />
           </div>
           <div className="LeftSide">
-            {/* <Roll left duration={2000}> */}
+            <Roll duration={2000}>
             <p>
                 I am a dedicated and hardworking US Army Veteran that recently found my passion in Software Development. I pride myself on my extreme attention to detail and love to experiement with code. I graduated from Bottega University with a certificate in Full Stack Development.
             </p>
-            {/* </Roll> */}
+            </Roll>
           </div>
         </div>
     </div>
--- src/components/pages/skills.js
+++ src/components/pages/skills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Zoom from 'react-reveal/Zoom';
+import { Zoom } from "react-awesome-reveal";
 
 const Skills = () => {
   return (
@@ -12,7 +12,7 @@ const Skills = () => {
             <h1 className="SkillsTitle">Tools</h1>
         </div>
         <div className="SkillsHero">
-            {/* <Zoom top cascade> */}
+            <Zoom top cascade duration={500}>
             <ul className="SkillsCol" style={{
                 borderRight: "1px solid white"
             }}>
@@ -21,8 +21,8 @@ const Skills = () => {
                 <li className="Skill">JavaScript</li>
                 <li className="Skill">ReactJS</li>
             </ul>
-            {/* </Zoom> */}
-            {/* <Zoom top cascade delay={1000}> */}
+            </Zoom>
+            <Zoom top cascade duration={500} delay={1000}>
             <ul className="SkillsCol">
                 <li className="Skill">Python3</li>
                 <li className="Skill">Flask</li>
@@ -30,8 +30,8 @@ const Skills = () => {
                 <li className="Skill">SQL Database</li>
                 <li className="Skill">Redis</li>
             </ul>
-            {/* </Zoom> */}
-            {/* <Zoom top cascade delay={2000}> */}
+            </Zoom>
+            <Zoom top cascade duration={500} delay={2000}>
             <ul className="SkillsCol" style={{
                 borderLeft: "1px solid white"
             }}>
@@ -40,7 +40,7 @@ const Skills = () => {
                 <li className="Skill">Git</li>
                 <li className="Skill">JSON</li>
             </ul>
-            {/* </Zoom> */}
+            </Zoom>
         </div>
     </div>
 
