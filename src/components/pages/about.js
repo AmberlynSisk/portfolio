8e1982e8767ad81f60e8ec6426ae2bb15d50268f
AmberlynSisk <amberlynsisk@yahoo.com>
2022-08-16 10:44:15 -0500

    updated resume and skills

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { RiDownloadLine } from 'react-icons/ri';
 import AboutPic from '../images/about.jpg';
 import { Roll } from "react-awesome-reveal";
-import Resume from '../downloads/Amberlyn_Sisk_Resume.PDF';
+import Resume from '../downloads/updated_resume_0822.pdf';
 
 const About = () => {
   return (
@@ -19,7 +19,7 @@ const About = () => {
           <div className="LeftSide">
             <Roll duration={2000}>
             <p>
-                I am a dedicated and hardworking US Army Veteran that recently found my passion in Software Development. I pride myself on my extreme attention to detail and love to experiement with code. I graduated from Bottega University with a certificate in Full Stack Development.
+                I am a dedicated and hardworking US Army Veteran that found my passion in Software Development. I pride myself on my extreme attention to detail and love to experiement with code. I graduated from Bottega University with a certificate in Full Stack Development and currently work as a Full Stack Developer for VacationCandy.com
             </p>
             </Roll>
           </div>
--- src/components/pages/skills.js
+++ src/components/pages/skills.js
@@ -6,11 +6,11 @@ const Skills = () => {
     <div className="SkillsContainer" id="skills">
         <div className="stars"></div>
         <div className="twinkling"></div>
-        <div className="SkillsTitlesWrapper">
+        {/* <div className="SkillsTitlesWrapper">
             <h1 className="SkillsTitle">Frontend</h1>
             <h1 className="SkillsTitle">Backend</h1>
             <h1 className="SkillsTitle">Tools</h1>
-        </div>
+        </div> */}
         <div className="SkillsHero">
             <Zoom top cascade duration={500}>
             <ul className="SkillsCol" style={{
@@ -20,12 +20,14 @@ const Skills = () => {
                 <li className="Skill">CSS/SCSS</li>
                 <li className="Skill">JavaScript</li>
                 <li className="Skill">ReactJS</li>
+                <li className="Skill">React Redux</li>
             </ul>
             </Zoom>
             <Zoom top cascade duration={500} delay={1000}>
             <ul className="SkillsCol">
                 <li className="Skill">Python3</li>
                 <li className="Skill">Flask</li>
+                <li className="Skill">PHP</li>
                 <li className="Skill">MongoDB</li>
                 <li className="Skill">SQL Database</li>
                 <li className="Skill">Redis</li>
@@ -39,6 +41,7 @@ const Skills = () => {
                 <li className="Skill">UI/UX</li>
                 <li className="Skill">Git</li>
                 <li className="Skill">JSON</li>
+                <li className="Skill">Docker</li>
             </ul>
             </Zoom>
         </div>
